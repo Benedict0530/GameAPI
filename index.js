fdca1f8cf2dd145a624690c922d5fe7ad0b2febf
Benedict0530 <mendozabenedict14@gmail.com>
2024-08-07 15:58:54 +0800

    Game API

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Use the cors middleware to allow all origins
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({ url: 'https://www.google.com' });
