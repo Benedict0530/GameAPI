6e2c0ba16395ae5f56da4087846020036bd1eff8
Benedict0530 <mendozabenedict14@gmail.com>
2024-08-07 15:44:48 +0800

    Game API

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.json({ url: 'https://www.google.com' });
 });
 
